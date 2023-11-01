534c88029f21f18847183968439bcaab6488e630
jadot3 <20b2093@ubd.edu.bn>
2023-11-01 21:52:25 +0800

    ja.

--- names.m
+++ names.m
@@ -17,7 +17,17 @@ xx_1 = spline(t_1, x_1, tt_1);
 yy_1 = spline(t_1, y_1, tt_1);
 
 % data points 2
+x = [1.5 6 6.5 5.5 3 2 4 6.5 10 ...
+    9 7 6 7 9 10 10 11 12];
+y = [8 9.5 8 0.5 0 3 5 4 4 ...
+    5 5 3 1 1 4 5 1 2];
 
+n_1 = length(x_1);
+t_1 = 0:n_1-1;
+tt_1 = 0:0.01:n_1-1;
+
+xx = spline(t, x, tt);
+yy = spline(t, y, tt);
 
 % data points 3
 
@@ -40,7 +50,17 @@ ylabel('y')
 grid on
 
 % subplot 2
-
+figure(2)
+plot(xx, yy, 'k', 'LineWidth', 1.5) % plot spline
+hold on
+plot(x, y, 'bo', 'MarkerFaceColor', 'b') % plot data points
+axis([-0.2 6 -0.2 4.5])
+grid on
+title('Spline Interpolation of Cursive Letter D')
+xlabel('x')
+ylabel('y')
+set(gca, 'FontSize', 10, 'LineWidth', 1)
+grid on
 % subplot 3
 
 % subplot 4
