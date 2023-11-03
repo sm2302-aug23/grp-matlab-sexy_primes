4992dd3256c33a84b5f0ef5bab0007eb38d66b38
fizah <141141649+nhfizhn@users.noreply.github.com>
2023-11-03 11:20:05 +0800

    update names.m
    plotted subplot 2

--- names.m
+++ names.m
@@ -17,17 +17,17 @@ xx_1 = spline(t_1, x_1, tt_1);
 yy_1 = spline(t_1, y_1, tt_1);
 
 % data points 2
-x = [1.5 6 6.5 5.5 3 2 4 6.5 10 ...
+x_2 = [1.5 6 6.5 5.5 3 2 4 6.5 10 ...
     9 7 6 7 9 10 10 11 12];
-y = [8 9.5 8 0.5 0 3 5 4 4 ...
+y_2 = [8 9.5 8 0.5 0 3 5 4 4 ...
     5 5 3 1 1 4 5 1 2];
 
-n_1 = length(x_1);
-t_1 = 0:n_1-1;
-tt_1 = 0:0.01:n_1-1;
+n_2 = length(x_2);
+t_2 = 0:n_2-1;
+tt_2 = 0:0.01:n_2-1;
 
-xx = spline(t, x, tt);
-yy = spline(t, y, tt);
+xx_2 = spline(t_2, x_2, tt_2);
+yy_2 = spline(t_2, y_2, tt_2);
 
 % data points 3
 
@@ -50,6 +50,14 @@ ylabel('y')
 grid on
 
 % subplot 2
+subplot(2,2,2), plot(xx_2, yy_2, 'k') % plot spline
+hold on
+plot(x_2, y_2, 'bo') % plot data points
+axis([0 13 -2 11])
+title('Spline Interpolation of Cursive Word Ja')
+xlabel('x')
+ylabel('y')
+grid on
 
 % subplot 3
 
