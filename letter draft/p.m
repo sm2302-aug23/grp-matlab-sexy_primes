5fa4c82d41e8da598fac27d072e59bba7e4b8512
jadot3 <20b2093@ubd.edu.bn>
2023-11-04 16:10:05 +0800

    draft letter coordinate

---
+++ letter draft/p.m
@@ -0,0 +1,23 @@
+x = [2 2.1 1.8 2.2 2.4 2.5 2.5 2.2];
+y = [3 3.2 2 3.2 3.3 3.2 3 2.8];
+
+n = length(x);
+t = 0:n-1; % Parametric coordinate t
+tt = 0:0.01:n-1; % More dense coordinate tt for spline interpolation
+
+% Compute spline interpolation
+xx = spline(t, x, tt);
+yy = spline(t, y, tt);
+
+% Plot settings
+figure(1)
+plot(xx, yy, 'k', 'LineWidth', 1.5) % plot spline
+hold on
+plot(x, y, 'bo', 'MarkerFaceColor', 'b') % plot data points
+axis([-0.2 6 -0.2 4.5])
+grid on
+title('Spline Interpolation of Cursive Letter D')
+xlabel('x')
+ylabel('y')
+set(gca, 'FontSize', 10, 'LineWidth', 1)
+grid on
---
+++ letter draft/z.m
@@ -0,0 +1,23 @@
+x = [2.2 2.0 2 2.6 3.0 2.8 2.1 2.2 3.0 3.4];
+y = [3.4 3.1 3 3.2 3.3 2.8 2.2 2 2.2 2.7];
+
+n = length(x);
+t = 0:n-1; % Parametric coordinate t
+tt = 0:0.01:n-1; % More dense coordinate tt for spline interpolation
+
+% Compute spline interpolation
+xx = spline(t, x, tt);
+yy = spline(t, y, tt);
+
+% Plot settings
+figure(1)
+plot(xx, yy, 'k', 'LineWidth', 1.5) % plot spline
+hold on
+plot(x, y, 'bo', 'MarkerFaceColor', 'b') % plot data points
+axis([-0.2 6 -0.2 4.5])
+grid on
+title('Spline Interpolation of Cursive Letter D')
+xlabel('x')
+ylabel('y')
+set(gca, 'FontSize', 10, 'LineWidth', 1)
+grid on
