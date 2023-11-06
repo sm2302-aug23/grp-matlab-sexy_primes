052001cdf763b8b53a594cb5b98e75d8f46bbb8a
fizah <141141649+nhfizhn@users.noreply.github.com>
2023-11-06 17:29:02 +0800

    m & mat files

---
+++ coordinates.m
@@ -0,0 +1,133 @@
+x_a = [1.4 1 0.5 1 1.4 1.3 1.5 1.8];
+y_a = [1 1.5 0.5 0.3 1 0.4 0.4 1];
+
+x_b = [0.9 1.2 1.2 0.9 0.8 0.85 1.1 1.3 1.2 1.15 1.35 1.6];
+y_b = [1 1.8 2.2 1.2 0.5 0.2 0.2 0.8 1.2 0.9 0.6 1];
+
+x_c = [1.4 1.3 1 0.9 1.25 1.6];
+y_c = [1 1.3 1 0.3 0.2 1];
+
+x_d = [1.3 1.1 0.7 0.8 1.2 1.6 1.6 1.3 1.5 1.8];
+y_d = [1 1.2 0.55 0.1 0.5 1.6 2.1 0.5 0.3 1];
+
+x_e = [0.7 1.4 0.8 0.7 1.25 1.8];
+y_e = [1 1.5 1.2 0.2 0.2 1];
+
+x_f = [1 1.25 1.2 0.9 0.9 1.05 1 1.2 1.6];
+y_f = [1 1.8 2.2 0.6 -0.1 0.1 1 0.5 1];
+
+x_g = [1.3 1 0.8 0.95 1.2 1.3 1.2 0.9 1.3 1.7];
+y_g = [1 1.1 0.5 0.2 0.5 1 -0.5 -0.8 0.1 1];
+
+x_h = [0.9 1.2 1.2 0.9 0.8 0.85 1 1.25 1.25 1.4 1.6];
+y_h = [1 1.8 2.2 1.2 0.5 0.2 0.9 0.9 0.25 0.25 1];
+
+x_i = [0.9 0.8 0.9 1.2];
+y_i = [1 0.5 0.2 1];
+
+x_j = [1.35 1.2 1 1 1.3 1.7];
+y_j = [1 -0.5 -0.8 -0.5 0 1];
+
+x_k = [0.9 1.2 1.2 0.9 0.8 0.85 1 1.25 1.15 1 1.25 1.6];
+y_k = [1 1.8 2.2 1.2 0.5 0.2 0.9 0.9 0.7 0.6 0.2 1];
+
+x_l = [0.9 1.2 1.2 0.9 0.8 0.9 1.2];
+y_l = [1 1.8 2.2 1.2 0.5 0.2 1];
+
+x_m = [0.5 0.7 0.5 0.8 1 0.9 1.1 1.3 1.2 1.5];
+y_m = [1 1.5 0.3 1.5 1.5 0.3 1.5 1.5 0.4 1];
+
+x_n = [0.5 0.7 0.5 0.8 1.3 1.2 1.3 1.5];
+y_n = [1 1.4 0.3 1.5 1.5 0.4 0.4 1];
+
+x_o = [0.5 0.8 0.6 1 1.2 0.8 1.1 1.5];
+y_o = [1 1.5 0.5 0.3 1.7 1.5 0.8 1];
+
+x_p = [0.5 0.8 0.5 0.8 1.2 1.1 0.8 1.1 1.6];
+y_p = [1 1 -0.5 1.5 1.7 0.8 1 0.8 1];
+
+x_q = [0.5 1.2 0.8 0.8 1.05 1.2 1 1.1 1.1 1.5];
+y_q = [1 1.7 1.5 0.8 0.9 1.7 -0.5 0.2 0.6 1];
+
+x_r = [0.5 0.7 0.6 1 1 1.5];
+y_r = [1 2 1.8 1.5 0.5 1];
+
+x_s = [0.5 0.8 0.75 0.85 1 0.7 0.5 0.8 1.3];
+y_s = [1 2 2 1.7 0.8 0.3 1 0.6 1];
+
+x_t = [0.5 1 0.8 0.7 1 0.65 0.6 1.2];
+y_t = [1 2.2 1.8 0.2 0.5 1.2 1 1];
+
+x_u = [0.5 0.6  0.65 1.05 1.2 1.25 1.6];
+y_u = [1 1.2  0.2 0.3 1.2 0.4 1];
+
+x_v = [0.5 0.6  0.65 1.05 1.2 1.25 1.6];
+y_v = [1 1.2  0.2 0.3 1.2 0.4 1];
+
+x_w = [0.5 0.6 0.75 0.9 1 1.05 1.2 1.3 1.35 1.5];
+y_w = [1 0.8 0.05 0.5 1.2 0 0.5 1.2 0.9 1];
+
+x_x = [0.5 1 0.6 1.2 0.9 1.2 1.6];
+y_x = [1 0.8 0 1.2 0.7 0 1];
+
+x_y = [0.8 1.3 1.1 1.1 1.3 1.6 1.8 1.9 1.6 0.9 0.8 1.5 2.4];
+y_y = [1 1.9 1.5 1.2 1 1.2 1.7 2 0.5 0.2 0.5 0.7 1];
+
+x_z = [0.9 0.7 0.7 1.3 1.7 1.5 0.8 0.9 1.7 2.1];
+y_z = [1 1.3 1.2 1.4 1.5 1 0.4 0.2 0.4 1];
+
+
+coordinates.x_a = x_a;
+coordinates.y_a = y_a;
+coordinates.x_b = x_b;
+coordinates.y_b = y_b;
+coordinates.x_c = x_c;
+coordinates.y_c = y_c;
+coordinates.x_d = x_d;
+coordinates.y_d = y_d;
+coordinates.x_e = x_e;
+coordinates.y_e = y_e;
+coordinates.x_f = x_f;
+coordinates.y_f = y_f;
+coordinates.x_g = x_g;
+coordinates.y_g = y_g;
+coordinates.x_h = x_h;
+coordinates.y_h = y_h;
+coordinates.x_i = x_i;
+coordinates.y_i = y_i;
+coordinates.x_j = x_j;
+coordinates.y_j = y_j;
+coordinates.x_k = x_k;
+coordinates.y_k = y_k;
+coordinates.x_l = x_l;
+coordinates.y_l = y_l;
+coordinates.x_m = x_m;
+coordinates.y_m = y_m;
+coordinates.x_n = x_n;
+coordinates.y_n = y_n;
+coordinates.x_o = x_o;
+coordinates.y_o = y_o;
+coordinates.x_p = x_p;
+coordinates.y_p = y_p;
+coordinates.x_q = x_q;
+coordinates.y_q = y_q;
+coordinates.x_r = x_r;
+coordinates.y_r = y_r;
+coordinates.x_s = x_s;
+coordinates.y_s = y_s;
+coordinates.x_t = x_t;
+coordinates.y_t = y_t;
+coordinates.x_u = x_u;
+coordinates.y_u = y_u;
+coordinates.x_v = x_v;
+coordinates.y_v = y_v;
+coordinates.x_w = x_w;
+coordinates.y_w = y_w;
+coordinates.x_x = x_x;
+coordinates.y_x = y_x;
+coordinates.x_y = x_y;
+coordinates.y_y = y_y;
+coordinates.x_z = x_z;
+coordinates.y_z = y_z;
+
+save('coordinates.mat', 'coordinates');
